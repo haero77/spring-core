a53707f9365a077ffb1a32318b239e08b6fae9cc
preferkim <shineecard5@gmail.com>
2022-11-06 04:15:00 +0900

    Refactor: 애플리케이션 구성이 어떻게 되어있는지 확인할 수 있도록 AppConfig 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,29 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixedDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import hello.core.repository.MemberRepository;
 import hello.core.repository.MemoryMemberRepository;
 
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixedDiscountPolicy();
     }
 }
