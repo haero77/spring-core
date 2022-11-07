50d68aaac5b7cae8f5421962d40f76fcac0a2e44
preferkim <shineecard5@gmail.com>
2022-11-08 05:42:08 +0900

    Test: 싱글톤 컨테이너
    "스프링 컨테이너의 99%는 싱글톤으로 사용한다."

--- src/test/java/hello/core/singleton/SingletonServiceTest.java
+++ src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -1,7 +1,11 @@
 package hello.core.singleton;
 
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +30,19 @@ class SingletonServiceTest {
          isSameAs: same, ==
          isEqualTo: equal, equals()
          */
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void spring_container() {
+        // given
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // when
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // then
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
